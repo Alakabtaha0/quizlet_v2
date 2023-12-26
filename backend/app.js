14235db9d29b9d523629c7cc14abe80ea791e7d4
Taha Al Akab <alakabtaha@gmail.com>
2023-12-26 03:21:36 +0000

    changing the file path

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.use('/api/v1/results', resultsRouter);
 app.use('/api/v1/answers', answerRouter);
 app.use('/api/v1/questions', questionRouter);
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     // Serve the static files from the React app
     res.sendFile(path.resolve(__dirname, 'public', 'build'));
 });
